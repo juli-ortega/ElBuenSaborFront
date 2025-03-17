45425d70487cd7d6d56ab096af6b1c31de67f25b
LucianoLosada23 <145129031+LucianoLosada23@users.noreply.github.com>
2025-03-16 22:34:59 -0300

    finish of the config redux

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Image from "next/image";
 import bs from "./../../public/bs3.svg"
+import Link from "next/link";
+import { StoreProvider } from "@/store/StoreProvider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,23 +18,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        <header>
-          <Navbar />
-        </header>
+    <StoreProvider>
+      <html lang="en">
+        <body>
+          <header>
+            <Navbar />
+          </header>
 
-        <main className="min-h-screen">{children}</main>
+          <main className="min-h-screen">{children}</main>
 
-        <footer>
-          <Footer />
-        </footer>
+          <footer>
+            <Footer />
+          </footer>
 
-        {/* Logo fijo en la esquina inferior derecha */}
-        <div className="fixed bottom-8 right-4 z-50 bg-black/60 rounded-full">
-          <Image src={bs} alt="Logo" width={40} height={40} />
-        </div>
-      </body>
-    </html>
+          <Link href={"/"}>
+          {/* Logo fijo en la esquina inferior derecha */}
+          <div className="fixed bottom-8 right-4 z-50 bg-black/60 rounded-full">
+            <Image src={bs} alt="Logo" width={40} height={40} />
+          </div>
+          </Link>
+
+        </body>
+      </html>
+    </StoreProvider>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -101,7 +101,7 @@ export default function Navbar() {
           </div>
         ) : (
           <div className="space-x-6">
-            <button className="font-display px-4 py-3 text-white cursor-pointer text-[12px] bg-terciario rounded-[10px] font-semibold uppercase tracking-widest hover:text-white hover:bg-secundario">
+            <button className="font-display px-4 py-3 text-white cursor-pointer text-[12px] bg-principal rounded-[10px] font-semibold uppercase tracking-widest hover:text-white hover:bg-secundario">
               Registrate
             </button>
             <button className="font-display uppercase cursor-pointer text-[14px] hover:bg-gris-claro py-3 px-4 rounded-[10px]">
